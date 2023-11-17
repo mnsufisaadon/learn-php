841947b0da03eb545533a9b94ad25b9177d3e364
mnsufisaadon <mn.sufisaadon@gmail.com>
2023-11-18 03:45:18 +0800

    refactor: enhance 404 logic and view

--- routes.php
+++ routes.php
@@ -8,12 +8,16 @@ $routes = [
 
 $uri = $_SERVER['REQUEST_URI'];
 
+function abort($code = 404) {
+    http_response_code($code);
+
+    require("controllers/{$code}.php");
+
+    die();
+}
+
 if(array_key_exists($uri, $routes)) {
     require($routes[$uri]);
 } else {
-    http_response_code(404);
-
-    echo "Sorry. Not Found.";
-
-    die();
+    abort();
 }
