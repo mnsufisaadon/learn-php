fc9e4d72330d8ffab331ee20c49b48692f4459d4
mnsufisaadon <mn.sufisaadon@gmail.com>
2023-11-18 04:22:08 +0800

    feat: connect with mysql

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
 
 require('functions.php');
-require('routes.php');
+// require('routes.php');
+
+$dsn = 'mysql:host=localhost;port=3306;dbname=learn_php';
+
+$pdo = new PDO($dsn, 'root');
+
+$statement = $pdo->prepare('select * from posts');
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>{$post['title']}</li>";
+}
+
