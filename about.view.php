be13e5e839b540c5a3927c42240bc4a40b0f8502
mnsufisaadon <mn.sufisaadon@gmail.com>
2023-11-17 22:35:43 +0800

    feat: add pages with tailwind template

--- about.view.php
+++ about.view.php
@@ -18,8 +18,8 @@
                 <div class="hidden md:block">
                     <div class="ml-10 flex items-baseline space-x-4">
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                        <a href="/" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
-                        <a href="/about.php" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
+                        <a href="/" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Home</a>
+                        <a href="/about.php" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">About</a>
                         <a href="/contact.php" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
                     </div>
                 </div>
@@ -119,12 +119,12 @@
 
         <header class="bg-white shadow">
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
+            <h1 class="text-3xl font-bold tracking-tight text-gray-900">About Us</h1>
             </div>
         </header>
         <main>
             <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-                <h1>Hello! This is home page.</h1>
+                <h1>You are in About Us page.</h1>
             </div>
         </main>
     </div>
--- index.php
+++ index.php
@@ -1,25 +1,3 @@
 <?php
 
-$books = [
-    [
-        'name' => 'Rich Dad Poor Dad',
-        'author' => 'Robert Kiyosaki',
-        'purchaseUrl' => 'http://localhost',
-    ],
-    [
-        'name' => 'Friends',
-        'author' => 'Richard',
-        'purchaseUrl' => 'http://example.com',
-    ],
-    [
-        'name' => 'How to Make Money',
-        'author' => 'Robert Kiyosaki',
-        'purchaseUrl' => 'http://localhost',
-    ],
-];
-
-$filteredBooks = array_filter($books, function($book) {
-    return $book['author'] === 'Robert Kiyosaki';
-});
-
 require "index.view.php";
