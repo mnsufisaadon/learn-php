99c1872c6d67bd67fe42bf2d36f5e19785955a78
mnsufisaadon <mn.sufisaadon@gmail.com>
2023-11-18 14:31:15 +0800

    refactor: move config to global config file

--- index.php
+++ index.php
@@ -4,13 +4,9 @@ require('functions.php');
 // require('routes.php');
 require('Database.php');
 
-$config = [
-    'host' => 'localhost',
-    'port' => 3306,
-    'dbname' => 'learn_php',
-];
+$config = require('config.php');
 
-$db = new Database($config);
+$db = new Database($config['database']);
 $posts = $db->query('select * from posts')->fetchAll();
 
 foreach ($posts as $post) {
