d94439f4cb6fb4ebb8d3e665741499e6c594b681
mnsufisaadon <mn.sufisaadon@gmail.com>
2023-11-18 12:08:19 +0800

    refactor: move database connection function to class

--- index.php
+++ index.php
@@ -3,14 +3,29 @@
 require('functions.php');
 // require('routes.php');
 
-$dsn = 'mysql:host=localhost;port=3306;dbname=learn_php';
+class Database {
 
-$pdo = new PDO($dsn, 'root');
+    public $connection;
 
-$statement = $pdo->prepare('select * from posts');
-$statement->execute();
+    public function __construct()
+    {
+        $dsn = 'mysql:host=localhost;port=3306;dbname=learn_php';
 
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+        $this->connection = new PDO($dsn, 'root');
+    }
+
+    public function query($query)
+    {
+        
+        $statement = $this->connection->prepare($query);
+        $statement->execute();
+        
+        return $statement;
+    }
+}
+
+$db = new Database();
+$posts = $db->query('select * from posts where id = 2')->fetchAll(PDO::FETCH_ASSOC);
 
 foreach ($posts as $post) {
     echo "<li>{$post['title']}</li>";
