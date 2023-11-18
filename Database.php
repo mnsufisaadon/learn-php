7b218699a78a5c596c3efc9267e5fd9861e9a6bc
mnsufisaadon <mn.sufisaadon@gmail.com>
2023-11-18 14:27:37 +0800

    feat: make Database configuration more dynamic and flexible

--- Database.php
+++ Database.php
@@ -4,11 +4,14 @@ class Database {
 
     public $connection;
 
-    public function __construct()
+    public function __construct($config, $username = 'root', $password = '')
     {
-        $dsn = 'mysql:host=localhost;port=3306;dbname=learn_php';
 
-        $this->connection = new PDO($dsn, 'root');
+        $dsn = 'mysql:'. http_build_query($config, '', ";");
+
+        $this->connection = new PDO($dsn, $username, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ]);
     }
 
     public function query($query)
--- index.php
+++ index.php
@@ -4,8 +4,14 @@ require('functions.php');
 // require('routes.php');
 require('Database.php');
 
-$db = new Database();
-$posts = $db->query('select * from posts where id = 2')->fetchAll(PDO::FETCH_ASSOC);
+$config = [
+    'host' => 'localhost',
+    'port' => 3306,
+    'dbname' => 'learn_php',
+];
+
+$db = new Database($config);
+$posts = $db->query('select * from posts')->fetchAll();
 
 foreach ($posts as $post) {
     echo "<li>{$post['title']}</li>";
