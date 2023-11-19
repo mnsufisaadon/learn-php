18ffb16c1f48769eeaf5f04f286971a7784dfab2
mnsufisaadon <mn.sufisaadon@gmail.com>
2023-11-19 12:10:42 +0800

    feat: bind parameter to execute query

--- index.php
+++ index.php
@@ -10,9 +10,11 @@ $db = new Database($config['database']);
 
 $id = $_GET['id'];
 
-$query = "select * from posts where id = $id";
+// calling variable directly inside query will cause a threat! like below
+// $query = "select * from posts where id = $id";
 
+$query = "select * from posts where id = :id";
 
-$posts = $db->query($query)->fetch();
+$posts = $db->query($query, ['id' => $id])->fetch();
 
 dd($posts);
