6dae043c2f949e287d9268ac966f717326e1f3fa
mnsufisaadon <mn.sufisaadon@gmail.com>
2023-11-20 00:07:33 +0800

    feat: handle unauthorized response

--- controllers/note.php
+++ controllers/note.php
@@ -6,15 +6,18 @@ $db = new Database($config['database']);
 $currentUserId = 1;
 
 
-$query = "select * from notes where id = :id and user_id = :user_id";
+$query = "select * from notes where id = :id";
 
 $note = $db->query($query, [
     'id' => $_GET['id'],
-    'user_id' => $currentUserId,
 ])->fetch();
 
 if (!$note) {
     abort();
+}
+
+if ($note['user_id'] !== $currentUserId) {
+    abort(403);
 }
 
 
