e5e6f9f6d62920cf2ee44eb4ac5e21c190379d15
mnsufisaadon <mn.sufisaadon@gmail.com>
2023-11-19 22:44:46 +0800

    feat: fetch only note from current user

--- controllers/note.php
+++ controllers/note.php
@@ -3,10 +3,12 @@
 $config = require('config.php');
 $db = new Database($config['database']);
 
+$currentUserId = 1;
 
-$query = "select * from notes where id = :id";
 
-$note = $db->query($query, [':id' => $_GET['id']])->fetch();
+$query = "select * from notes where id = :id and user_id = :user_id";
+
+$note = $db->query($query, [':id' => $_GET['id'], ':user_id' => $currentUserId])->fetch();
 
 $banner = "Note";
 
