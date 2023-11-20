dda18cfe29328e3ff39314be5711662b85843b9b
mnsufisaadon <mn.sufisaadon@gmail.com>
2023-11-20 09:37:24 +0800

    feat: add form validation to the server side

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -5,11 +5,26 @@ $db = new Database($config['database']);
 
 $banner = "Create new note";
 
+$errors = [];
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-   $db->query("INSERT INTO `learn_php`.`notes` (`body`, `user_id`) VALUES (:body, :user_id)", [
-    'body' => $_POST['body'],
-    'user_id' => 1,
-   ]);
+
+    if (strlen($_POST['body']) === 0) {
+        // we need to add validation to the server side so user cannot bypass the client side.
+        // eg: curl -X POST http://localhost:8888/notes/create -d 'body='
+        $errors['body'] = 'A body is required.';
+    }
+
+    if (strlen(($_POST['body'])) > 1000) {
+        $errors['body'] = 'The body can not be more than 1000 characters.';
+    }
+
+    if (empty($errors)) {
+        $db->query("INSERT INTO `learn_php`.`notes` (`body`, `user_id`) VALUES (:body, :user_id)", [
+            'body' => $_POST['body'],
+            'user_id' => 1,
+        ]);
+    }
 }
 
 require "views/note-create.view.php";
