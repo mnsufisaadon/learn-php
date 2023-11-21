32428fb2fd5e2beb42abb202a0c19137462c2f87
mnsufisaadon <mn.sufisaadon@gmail.com>
2023-11-21 21:02:23 +0800

    feat: trim any white space before validate

--- Validator.php
+++ Validator.php
@@ -4,6 +4,7 @@ class Validator {
     //this is pure function, so can use static --- no $this exist
     public static function string($value, $min = 1, $max = INF)
     {
+        $value = trim($value);
         return strlen($value) >= $min && strlen($value) <= $max;
     }
 
