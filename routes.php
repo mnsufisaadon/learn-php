c231deba2bcb340af388634874e2c87e460c9341
mnsufisaadon <mn.sufisaadon@gmail.com>
2023-11-27 15:56:00 +0800

    feat: restrict notes only for authenticated user

--- routes.php
+++ routes.php
@@ -4,7 +4,7 @@ $router->get('/', 'controllers/index.php');
 $router->get('/about', 'controllers/about.php');
 $router->get('/contact', 'controllers/contact.php');
 
-$router->get('/notes', 'controllers/notes/index.php');
+$router->get('/notes', 'controllers/notes/index.php')->only('auth');
 
 $router->get('/notes/create', 'controllers/notes/create.php');
 $router->post('/notes', 'controllers/notes/store.php');
