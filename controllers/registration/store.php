84f9b9ede85253a010eb1bfca5735cd7e8182e2a
mnsufisaadon <mn.sufisaadon@gmail.com>
2023-11-27 08:11:05 +0800

    feat: hash password when register

--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -46,7 +46,7 @@ if ($isEmailExist) {
 
     $db->query('INSERT INTO users (email, password) VALUES (:email, :password)', [
         'email' => $email,
-        'password' => $password,
+        'password' => password_hash($password, PASSWORD_BCRYPT),
     ]);
 
 }
